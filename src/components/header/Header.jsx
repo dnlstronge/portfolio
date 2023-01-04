73437ff98283c6cb6ee33b45db465a17d66efb5c
Dan Stronge <dnlstronge@gmail.com>
2023-01-04 14:53:18 +0000

    added buttons and downloadable cv (placeholder)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,19 @@
 import './header.css'
-
+import CTA from './CTA'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hi, I'm</h5>
+        <h1>Dan Stronge</h1>
+        <h5 className="text-light">Software Developer</h5>
+        <CTA />
+
+        <div className='me'></div>
+      </div>
+    </header>
   )
 }
 
