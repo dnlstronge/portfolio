08944670080b4c3fbb97903f8a00e06324e3a103
Dan Stronge <dnlstronge@gmail.com>
2023-01-05 10:40:06 +0000

    create: add icons to navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import './nav.css'
-
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineBook } from 'react-icons/ai'
+import { TbHammer } from 'react-icons/tb'
+import { AiOutlinePhone } from 'react-icons/ai'
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><AiOutlineBook/></a>
+      <a href="#portfolio"><TbHammer/></a>
+      <a href="#contact"><AiOutlinePhone/></a>
+    </nav>
   )
 }
 
