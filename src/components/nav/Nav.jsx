bc7bbf97c61c2d7ee238d4c20cd8867aa25a2cf0
Dan Stronge <dnlstronge@gmail.com>
2023-01-05 11:36:02 +0000

    edit icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
 import { AiOutlineBook } from 'react-icons/ai'
 import { TbHammer } from 'react-icons/tb'
-import { AiOutlinePhone } from 'react-icons/ai'
+import { BiMessageDetail } from 'react-icons/bi'
 import React from 'react'
 
 const Nav = () => {
@@ -13,7 +13,7 @@ const Nav = () => {
       <a href="#about"><AiOutlineUser/></a>
       <a href="#experience"><AiOutlineBook/></a>
       <a href="#portfolio"><TbHammer/></a>
-      <a href="#contact"><AiOutlinePhone/></a>
+      <a href="#contact"><BiMessageDetail/></a>
     </nav>
   )
 }
